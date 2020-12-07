86958bd82cdfe6fbec685ee831d61201259f6c27
Gaetan Frejoux <gaetanfrejoux@gmail.com>
2020-12-07 14:27:23 +0100

    update

--- src/master.c
+++ src/master.c
@@ -11,12 +11,23 @@
 #include "master_client.h"
 #include "master_worker.h"
 
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 /************************************************************************
  * Données persistantes d'un master
  ************************************************************************/
 
 // on peut ici définir une structure stockant tout ce dont le master
 // a besoin
+typedef struct{
+	int highestPrime;
+	int highestAskedNumber;
+	int howManyCalculatedPrime;
+} *masterStats;
 
 
 /************************************************************************
@@ -35,8 +46,35 @@ static void usage(const char *exeName, const char *message)
 /************************************************************************
  * boucle principale de communication avec le client
  ************************************************************************/
-void loop(/* paramètres */)
+void loop(masterStats m)
 {
+	int tcm = open("tubeClientMaster",O_RDONLY); //ouverture en mode lecture
+    int tmc = open("tubeMasterClient",O_WRONLY); //ouverture en mode écriture
+    int valeur;
+    while(true){
+		if(read(tcm,&valeur,sizeof(int))==1)
+		{
+			if(valeur == ORDER_STOP)
+			{
+				//TODO
+			}
+			
+			if(valeur == ORDER_COMPUTE_PRIME)
+			{
+				//TODO
+			}
+			
+			if(valeur == ORDER_HOW_MANY_PRIME)
+			{
+				//TODO
+			}
+			
+			if(valeur == ORDER_HIGHEST_PRIME)
+			{
+				//TODO
+			}
+		}
+	}
     // boucle infinie :
     // - ouverture des tubes (cf. rq client.c)
     // - attente d'un ordre du client (via le tube nommé)
@@ -75,11 +113,27 @@ int main(int argc, char * argv[])
         usage(argv[0], NULL);
 
     // - création des sémaphores
+    
+    
     // - création des tubes nommés
+    mkfifo("tubeClientMaster",0600); //tube client vers master
+    mkfifo("tubeMasterClient",0600); //tube master vers client
+    
+    
     // - création du premier worker
-
+    int son = fork();
+    if(son == 0){
+		execv("worker",NULL);
+	}
+	
+	
+	masterStats ms;
+	ms->highestPrime = 2;
+	ms->highestAskedNumber = 1; 
+	ms->howManyCalculatedPrime = 0; //ou 1 ?
     // boucle infinie
-    loop(/* paramètres */);
+    
+    loop(ms);
 
     // destruction des tubes nommés, des sémaphores, ...
 
