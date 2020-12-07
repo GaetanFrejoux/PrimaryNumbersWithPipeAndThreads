a820673c952803895fbe1fef2fb4391b25c06a5c
Mathieu Niord <69221076+MathieuNiord@users.noreply.github.com>
2020-12-07 20:25:43 +0100

    J'ai ajouté plusieurs fonctions

--- src/master_client.h
+++ src/master_client.h
@@ -1,11 +1,13 @@
 #ifndef CLIENT_CRIBLE
 #define CLIENT_CRIBLE
 
-
 // On peut mettre ici des éléments propres au couple master/client :
 //    - des constantes pour rendre plus lisible les comunications
 //    - des fonctions communes (création tubes, écriture dans un tube,
 //      manipulation de sémaphores, ...)
+
+
+/*** === CONSTANTES === ***/
 
 // ordres possibles pour le master
 #define ORDER_NONE                0
@@ -19,26 +21,41 @@
 // pour masquer l'implémentation
 
 
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-
-// identifiant pour le deuxième paramètre de ftok
+/*Identifiant pour le deuxième paramètre de ftok*/
 #define PROJ_ID 1
 
 
+/*** === FONCTIONS === ***/
 
 
+//TUBES
+
+int myopen(const char *pathname, int flags, mode_t mode);
+ssize_t myread(int fd, void *buf, size_t count);
+ssize_t mywrite(int fd, const void *buf, size_t count);
+
+int mymkfifo(const char *pathname, mode_t mode);
 
 
+//SÉMAPHORES
 
-key_t getKey();
-int semCreator();
-int semGet();
-void semSetVal(int id,int val);
-void semOperation(int id , struct sembuf op , int time);
+key_t getKey(const char *pathname, int proj_id);
+
+int semCreator(key_t key);
+int semGet(key_t key, int nsems, int semflg);
+
+void semCtl(int semid, int semnum, int cmd);
+void semSetVal(int semid, int val);
+void semDestruct(int semid);
+
+void prendre(int semid);
+void vendre(int semid);
+void attendre(int semid);
+
+
+//THREADS
+
+int mycreate(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
+int myjoin(pthread_t thread, void **retval);
+
 #endif
