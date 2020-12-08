41d9bab443832ba82f78fa084ef1f9814b799ee4
Mathieu Niord <69221076+MathieuNiord@users.noreply.github.com>
2020-12-08 22:40:09 +0100

    include de utils

--- src/master_client.h
+++ src/master_client.h
@@ -6,7 +6,9 @@
 //    - des fonctions communes (création tubes, écriture dans un tube,
 //      manipulation de sémaphores, ...)
 
+/*** === INCLUDE === ***/
 
+#include "utils.h"
 
 /*** === CONSTANTES === ***/
 
@@ -27,36 +29,5 @@
 
 
 /*** === FONCTIONS === ***/
-
-
-//TUBES
-
-int myopen(const char *pathname, int flags);
-ssize_t myread(int fd, void *buf, size_t count);
-ssize_t mywrite(int fd, const void *buf, size_t count);
-
-int mymkfifo(const char *pathname, mode_t mode);
-
-
-//SÉMAPHORES
-
-key_t getKey(const char *pathname, int proj_id);
-
-int semCreator(key_t key);
-int semGet(key_t key);
-
-void semCtl(int semid, int semnum, int cmd);
-void semSetVal(int semid, int val);
-void semDestruct(int semid);
-
-void prendre(int semid);
-void vendre(int semid);
-void attendre(int semid);
-
-
-//THREADS
-
-int mycreate(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
-int myjoin(pthread_t thread, void **retval);
 
 #endif
